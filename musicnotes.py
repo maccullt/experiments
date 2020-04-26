f328e17c5d0c462351fa8348bb10a9281fc6c01b
maccullt <maccullt@gmail.com>
2020-04-25 22:12:19 -0400

    use f strings

--- musicnotes.py
+++ musicnotes.py
@@ -34,6 +34,13 @@ def add_interval_to_note(interval, i_note):
 def add_semitones_to_note(semitone_count, i_note):
     return (i_note + semitone_count) % CHROMATIC_NOTE_COUNT
 
+def make_cycle(interval):
+    cycle = [i_c_note]
+    i_next_note = add_interval_to_note( interval, cycle[-1] )
+    while i_next_note != i_c_note:
+        cycle.append(i_next_note)
+        i_next_note = add_interval_to_note( interval, cycle[-1] )
+    return cycle
 
 MAJOR_SCALE_INTERVALS = [2,2,1,2,2,2,1]
 
@@ -41,8 +48,16 @@ MAJOR_SCALE_INTERVALS = [2,2,1,2,2,2,1]
 def get_major_scale_starting_at( i_note ):
     notes = [SHARP_NOTES[i_note]]
     for i_interval in MAJOR_SCALE_INTERVALS:
-        i_note = add_semitones_to_note(i_interval, i_note) 
-        notes.append(SHARP_NOTES[i_note])
+        i_note = add_semitones_to_note(i_interval, i_note)
+        if  SHARP_NOTES[i_note] != FLAT_NOTES[i_note]:            
+            # this note is a sharp check the degree
+            if len(notes) > 0 and notes[-1] == SHARP_NOTES[i_note -1]:
+                notes.append(FLAT_NOTES[i_note])
+            else:    
+                notes.append(SHARP_NOTES[i_note])
+        else:    
+            notes.append(SHARP_NOTES[i_note])
+
     return notes
 
 
@@ -57,31 +72,26 @@ for interval in Interval:
     i_second_note = add_interval_to_note( interval, i_c_note)
     from_note = SHARP_NOTES[i_c_note]
     to_note = SHARP_NOTES[i_second_note]
-    print( "{0:s} plus {1:s} is {2:s} ({3:d} semitones)".format(from_note, interval.name, to_note, interval.value) )
-
-
+    print( f"{from_note} plus {interval.name} is {to_note} ({interval.value} semitones)")
 
 for num_semitones in range(CHROMATIC_NOTE_COUNT * 2 +1):    
     i_second_note = add_semitones_to_note(num_semitones, i_c_note) 
     from_note = SHARP_NOTES[i_c_note]
     to_note = SHARP_NOTES[i_second_note]
     to_note_flat = FLAT_NOTES[i_second_note]
-    print( "{0:s} plus {1:d} semitones is {2:s}/{3:s}".format(from_note,  num_semitones, to_note, to_note_flat) )
-
-
+    print( f'{from_note} plus {num_semitones} semitones is {to_note}/{to_note_flat}')
 
 for i_current_note in range(CHROMATIC_NOTE_COUNT ):
-    print('Major scale starting at {0:s}: {1:s}'.format(
-        SHARP_NOTES[i_current_note], 
-        ','.join( get_major_scale_starting_at(i_current_note) )))
+    print(f"Major scale starting at {SHARP_NOTES[i_current_note]}: {','.join( get_major_scale_starting_at(i_current_note))}")
 
-
-cycle_of_fourths = [i_c_note]
-i_next_note = add_interval_to_note( Interval.PERFECT_FOURTH, cycle_of_fourths[-1] )
-
-while i_next_note != i_c_note:
-    cycle_of_fourths.append(i_next_note)
-    i_next_note = add_interval_to_note( Interval.PERFECT_FOURTH, cycle_of_fourths[-1] )
-
+cycle_of_fourths = make_cycle(Interval.PERFECT_FOURTH)
 cycle_notes = [FLAT_NOTES[i] for i in cycle_of_fourths]
-print('cycle of fourths: {0:s}'.format(', '.join(cycle_notes)))   
+print(f"cycle of fourths: {', '.join(cycle_notes)}")   
+
+cycle_of_fifths = make_cycle(Interval.PERFECT_FIFTH)
+cycle_notes = [FLAT_NOTES[i] for i in cycle_of_fifths]
+print(f"cycle of fifths: {', '.join(cycle_notes)}")   
+
+for i_key in cycle_of_fifths:
+    major_scale = get_major_scale_starting_at(i_key)
+
